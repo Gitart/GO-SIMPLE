ee7ed0ab56e47913a8344c3e6fd56e78be6a1d32
Arthur Savage <alerc@meta.ua>
2018-09-10 12:54:11 +0300

    Update route.go

--- wizzard/route.go
+++ wizzard/route.go
@@ -1,3 +1,34 @@
+//   Title       : Test Route
+//   Remark      : Тестирование роутера справочных таблиц
+// 	 Date        : 15-01-2016 18:02
+//   Url         : tst/route/
+func Test_route(w http.ResponseWriter, req *http.Request) {
+	 fmt.Fprintf(w, Srouter("Persons") + "\n")
+	 fmt.Fprintf(w, Srouter("Cas")     + "\n")
+	 fmt.Fprintf(w, Srouter("Pns")     + "\n")
+	 fmt.Fprintf(w, Srouter("Other")   + "\n")
+	 fmt.Fprintf(w, Srouter("Cls")     + "\n")
+	 fmt.Fprintf(w, Srouter("")        + "---\n")
+}
+
+//   Title       : Направление в таблицу область всех справочников заранеее опредленных для сервиса
+//   Remark      : Второй не основной вариант
+// 	 Date        : 15-01-2016 18:02
+func S_router(S string){
+   M :="Persons2"
+   D :=[]string{"Persons","Cashreasons"}
+
+    for _, r:= range D  {
+	   if r==M{
+	      fmt.Println(r)
+	      break
+	   }else{
+	      fmt.Println("No")
+	      break
+	   }
+    }
+}
+
 //   Title       : Направление в таблицу (область) всех справочников заранеее опредленных для сервиса.
 // 	 Date        : 15-01-2016 18:02
 //                 Определить таблицу в котрую будут попадать данные  в зависмости от имени входной таблицы.
