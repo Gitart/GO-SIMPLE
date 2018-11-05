7963a69b8d5691cd5bb329cc14f468a1c8a3800f
Arthur Savage <alerc@meta.ua>
2018-11-05 11:41:53 +0200

    Create builder.go

---
+++ build/builder.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+  ub := &UserBuilder{}
+  user := ub.
+    Name("Michael Scott").
+    Role("manager").
+    Build()
+  fmt.Println(user)
+}
+
+type User struct {
+  Name      string
+  Role      string
+  MinSalary int
+  MaxSalary int
+}
+
+type UserBuilder struct {
+  User
+}
+
+func (ub *UserBuilder) Build() User {
+  return ub.User
+}
+
+func (ub *UserBuilder) Name(name string) *UserBuilder {
+  ub.User.Name = name
+  return ub
+}
+
+func (ub *UserBuilder) Role(role string) *UserBuilder {
+  // verify the role is valid
+  if role == "manager" {
+    ub.User.MinSalary = 20000
+    ub.User.MaxSalary = 60000
+  }
+  ub.User.Role = role
+  return ub
+}
