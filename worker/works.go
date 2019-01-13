30708d3b832449e2b79236d271dec9814a4085fb
Arthur Savage <alerc@meta.ua>
2019-01-13 18:23:00 +0200

    Create works.go

---
+++ worker/works.go
@@ -0,0 +1,26 @@
+type Worker struct {
+    stop chan struct{}
+    wg   sync.WaitGroup
+}
+
+func (w *Worker) run() {
+    defer w.wg.Done()
+    defer fmt.Println("All done")
+    for {
+        select {
+        case <-w.stop:
+            return
+        case <-time.After(100 * time.Millisecond):
+            fmt.Println("Waited 100ms")
+        }
+    }
+}
+
+func main() {
+    w := &Worker{stop: make(chan struct{})}
+    w.wg.Add(1)
+    go w.run()
+    time.Sleep(300 * time.Millisecond)
+    close(w.stop)
+    w.wg.Wait()
+}
