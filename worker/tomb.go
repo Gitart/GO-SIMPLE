a843298e902daf70965a4ebdd8059c7f8c7b17f6
Arthur Savage <alerc@meta.ua>
2019-01-13 18:23:30 +0200

    Create tomb.go

---
+++ worker/tomb.go
@@ -0,0 +1,24 @@
+type Worker struct {
+    tomb.Tomb
+}
+
+func (w *Worker) run() {
+    defer w.Tomb.Done()
+    defer fmt.Println("All done")
+    for {
+        select {
+        case <-w.Tomb.Dying():
+            return
+        case <-time.After(100 * time.Millisecond):
+            fmt.Println("Waited 100ms")
+        }
+    }
+}
+
+func main() {
+    w := &Worker{}
+    go w.run()
+    <-time.After(300 * time.Millisecond)
+    w.Tomb.Kill(nil) // normal exit
+    w.Tomb.Wait()
+}
