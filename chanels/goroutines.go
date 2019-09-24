6b2d6bcf5148aebde515c2b79547d0b622cf577c
Arthur Savage <alerc@meta.ua>
2019-09-24 17:26:13 +0300

    Create goroutines.go

---
+++ chanels/goroutines.go
@@ -0,0 +1,41 @@
+## Поймать значения из Goroutines
+#### Самый естественный способ получить значение из программы - каналы. Каналы - это трубы, которые соединяют параллельные программы. Вы можете отправлять значения в каналы из одной процедуры и получать эти значения в другую процедуру или в синхронной функции.
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+)
+
+// WaitGroup is used to wait for the program to finish goroutines.
+var wg sync.WaitGroup
+
+func responseSize(url string, nums chan int) {
+	// Schedule the call to WaitGroup's Done to tell goroutine is completed.
+	defer wg.Done()
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Send value to the unbuffered channel
+	nums <- len(body)
+}
+
+func main() {
+	nums := make(chan int) // Declare a unbuffered channel
+	wg.Add(1)
+	go responseSize("https://www.golangprograms.com", nums)
+	fmt.Println(<-nums) // Read the value from unbuffered channel
+	wg.Wait()
+	close(nums) // Closes the channel
+}
