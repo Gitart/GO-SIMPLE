4ececddb0f4f50abe97accb1698a71e88336fbf9
Arthur Savage <alerc@meta.ua>
2020-12-11 05:08:38 +1100

    Update read_csv.go

--- csv/read_csv.go
+++ csv/read_csv.go
@@ -31,3 +31,48 @@ func main() {
 		fmt.Printf("post with id %s is titled %q: %q\n", post[0], post[1], post[2])
 	}
 }
+
+
+
+
+
+package main
+
+import (
+	//"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	// Open the file
+	csvfile, err := os.Open("input.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile)
+	//r := csv.NewReader(bufio.NewReader(csvfile))
+
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Question: %s Answer %s\n", record[0], record[1])
+	}
+}
+
+
+
+
+
