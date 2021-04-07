8c5c8daf52c27fcd9d9157455be6e0d1bcf4ad51
Arthur Savage <alerc@meta.ua>
2021-04-07 17:23:11 +0300

    Create number.go

---
+++ convert/number.go
@@ -0,0 +1,9 @@
+func FormatPrice(price interface{}) string {
+	switch price.(type) {
+	case float32, float64:
+		return fmt.Sprintf("%0.2f", price)
+	case int, uint, int32, int64, uint32, uint64:
+		return fmt.Sprintf("%d.00", price)
+	}
+	return ""
+}
