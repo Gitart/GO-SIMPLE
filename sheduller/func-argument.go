667d8f2dd0c7ae146e22d1da38e6dd3008f8fbcc
Arthur Savage <alerc@meta.ua>
2021-08-27 11:57:20 +0300

    Update func-argument.go

--- sheduller/func-argument.go
+++ sheduller/func-argument.go
@@ -1,3 +1,5 @@
+## Sample
+```go
 package main
 
 import "fmt"
@@ -17,3 +19,30 @@ func main() {
 	test(myfn1, 123)
 	test(myfn2, 321)
 }
+```
+
+```go
+package main
+
+    import "fmt"
+
+    func plusTwo() (func(v int) (int)) {
+        return func(v int) (int) {
+            return v+2
+        }
+    }
+
+    func plusX(x int) (func(v int) (int)) {
+       return func(v int) (int) {
+           return v+x
+       }
+    }
+
+    func main() {
+        p := plusTwo()
+        fmt.Printf("3+2: %d\n", p(3))
+
+        px := plusX(3)
+        fmt.Printf("3+3: %d\n", px(3))
+    }
+    ```
