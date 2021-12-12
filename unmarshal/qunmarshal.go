23dab8913437d4fd8cc44b664c9983767eea284f
Arthur Savage <alerc@meta.ua>
2021-12-12 18:31:03 +0200

    Create qunmarshal.go

---
+++ unmarshal/qunmarshal.go
@@ -0,0 +1,32 @@
+package mypkg
+
+import (
+	"fmt"
+	"io"
+)
+
+type YesNo bool
+
+// UnmarshalGQL implements the graphql.Unmarshaler interface
+func (y *YesNo) UnmarshalGQL(v interface{}) error {
+	yes, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("YesNo must be a string")
+	}
+
+	if yes == "yes" {
+		*y = true
+	} else {
+		*y = false
+	}
+	return nil
+}
+
+// MarshalGQL implements the graphql.Marshaler interface
+func (y YesNo) MarshalGQL(w io.Writer) {
+	if y {
+		w.Write([]byte(`"yes"`))
+	} else {
+		w.Write([]byte(`"no"`))
+	}
+}
