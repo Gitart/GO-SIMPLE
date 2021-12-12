7855a92b43ea86f9fa6223d037eafc70f08113d4
Arthur Savage <alerc@meta.ua>
2021-12-12 15:34:57 +0200

    Update timecheck.go

--- timer/timecheck.go
+++ timer/timecheck.go
@@ -1,3 +1,4 @@
+// https://go.dev/play/p/ZqW7PpBKCLV
 package main
 
 import (
@@ -6,13 +7,9 @@ import (
 )
 
 func main() {
-
 	start := time.Now()
-
 	time.Sleep(time.Millisecond * 1223)
-
 	secs := time.Since(start).Milliseconds()
 	out := fmt.Sprintf("%d ", secs)
 	fmt.Println(out)
-
 }
