db0be02ad81f29bb269a105b491e57a64019e720
Arthur Savage <alerc@meta.ua>
2021-12-19 14:40:19 +0200

    Create context-done-wait.go

---
+++ context/context-done-wait.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+import "time"
+import "context"
+
+
+
+func main() {
+
+    // Ожидание процесса
+    dur := time.Millisecond*1000
+    ctx := context.Background() 
+    ctx, cancel :=context.WithTimeout(ctx, dur)
+    defer cancel()
+
+    doReguest(ctx, "Ok")
+}
+
+
+// *************************************
+// Do thamting
+// *************************************
+func doReguest(ctx context.Context, key string) {
+
+// Do thamthing ...
+
+select {
+        // Ожидание после котрого выход
+        case <-time.After(1500*time.Millisecond):
+            fmt.Println("timeout отмена")
+            return
+        case<- ctx.Done():
+            fmt.Println("done ")
+} 
+
+// time.Sleep(time.Millisecond*1500)
+
+
+fmt.Println("KEY :",key)
+
+}
