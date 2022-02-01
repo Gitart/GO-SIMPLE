7c45b2c688cfbabb3dfe5444323177b15c40b43e
Arthur Savage <alerc@meta.ua>
2022-02-01 10:05:06 +0200

    Update main.go

--- structure/incapsule/main.go
+++ structure/incapsule/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "encoding/json"
 
 // SETT
 type Sett struct {
@@ -9,7 +10,6 @@ type Sett struct {
        Total      Total
        Basic      Basic
 }
-
 
 type System struct {
        Title string
@@ -28,9 +28,11 @@ type Basic struct {
      Incapsule     
 }
 
-func (b Basic) Long() string {return b.Title}
-func (b Basic) Incs() Incapsule {return b.Incapsule}
+func (b Basic) Long() string {
+      return b.Title
+}
 
+func (b Basic) Incs() Incapsule {return b.Incapsule}
 
 // INCAPSULE
 type Incapsule struct {
@@ -52,14 +54,21 @@ type Subcapsule struct {
 
 func (s Subcapsule) Teradata() Terra {return s.Terra}
 
-
 // TERRAA
 type Terra struct {
      Title string
      Date  string
 }
 
-func (t Terra) Titles() string {return "TITLE TERRA DATA :" + t.Title}
+func (t Terra) Titles() string {
+      return "TITLE TERRA DATA :" + t.Title
+}
+
+// Terra show json
+func (m Terra) ShowJSONStatistics() {
+      data, _ := json.MarshalIndent(m, "", "  ")
+      fmt.Printf("Terra data : %s\n", string(data))
+}
 
 
 // main
@@ -70,18 +79,19 @@ func main(){
      Dat.System.Title                            = "System: Title"
      Dat.Basic.Title                             = "Bisaik title get"
      Dat.Basic.Incapsule.Date                    = "Дата в инкапсуле"
-     Dat.Basic.Incapsule.Title                   = "TItle в инкапсуле"
+     Dat.Basic.Incapsule.Title                   = "Title в инкапсуле"
      Dat.Basic.Incapsule.Subcapsule.Title        = "SUBCAPSULA: TItle в инкапсуле"
-     Dat.Basic.Incapsule.Subcapsule.Terra.Title  = "TerraDATAA: TItle в инкапсуле"
+     Dat.Basic.Incapsule.Subcapsule.Terra.Title  = "TerraDATA:  TItle в инкапсуле"
 
      fmt.Println(Dat)
      fmt.Println(Dat.Basic.Long())
      fmt.Println(Dat.Basic.Title)
 
-
      fmt.Println(Dat.Basic.Incs().Duble())
      fmt.Println(Dat.Basic.Incs().Rsub())
      fmt.Println(Dat.Basic.Incs().Rsub().Teradata())
-     fmt.Println("TITLE DATA:",   Dat.Basic.Incs().Rsub().Teradata().Title)
-     fmt.Println("FUNTION DATA:", Dat.Basic.Incs().Rsub().Teradata().Titles())
+     fmt.Println("TITLE DATA:",    Dat.Basic.Incs().Rsub().Teradata().Title)
+     fmt.Println("FUNCTION DATA:", Dat.Basic.Incs().Rsub().Teradata().Titles())
+
+     Dat.Basic.Incs().Rsub().Teradata().ShowJSONStatistics()
 }
