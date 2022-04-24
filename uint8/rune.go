9b285a3be4b75247bc7147f731dfe03e9163ce9d
Arthur Savage <alerc@meta.ua>
2022-04-24 13:26:01 +0300

    Create rune.go

---
+++ uint8/rune.go
@@ -0,0 +1,24 @@
+// translate Esc[X
+func escapeExKey(key *escapeKeyPair) rune {
+	var r rune
+	switch key.typ {
+	case 'D':
+		r = CharBackward
+	case 'C':
+		r = CharForward
+	case 'A':
+		r = CharPrev
+	case 'B':
+		r = CharNext
+	case 'H':
+		r = CharLineStart
+	case 'F':
+		r = CharLineEnd
+	case '~':
+		if key.attr == "3" {
+			r = CharDelete
+		}
+	default:
+	}
+	return r
+}
