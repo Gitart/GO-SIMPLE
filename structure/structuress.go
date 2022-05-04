5343df7cb8b81d4885cde484cac85a267a025514
Arthur Savage <alerc@meta.ua>
2022-05-04 13:09:42 +0300

    Update structuress.go

--- structure/structuress.go
+++ structure/structuress.go
@@ -18,21 +18,27 @@ t:=[]struct{
             Structure string
             Other     string
             Tg        Y 
-
            }{
-              {"Kiev",     "Kievskaya",   "Структура 1", "Address 1",  Y{"Jonh",   "Kolobov", []string{"Arts",  "Traveling"}} }, 
-              {"Kotlova",  "Obolon",      "Structur 2",  "Address 2",  Y{"Inecca", "Deeva",   []string{"Photos", "Reading", "Other Hoobyt"}}},
-              {"Kолосова", "Pechorsk",    "Structur 3",  "Address 2",  Y{"Inecca", "Deeva",   []string{"Photos", "Reading", "Other Hoobyt","Flyting", "Productive"}}},
+              {"Kiev",     "Kievskaya",     "Структура 1", "Address 1",  Y{"Jonh",   "Kolobov", []string{"Arts",  "Traveling"}} }, 
+              {"Kotlova",  "Obolon",        "Structur 2",  "Address 2",  Y{"Inecca", "Deeva",   []string{"Photos", "Reading", "Other Hoobyt"}}},
+              {"август",   "Pechorsk",      "Structur 3",  "Address 3",  Y{"Inecca", "Deeva",   []string{"Photos", "Reading", "Other Hoobyt","Flyting", "Productive"}}},
+              {"Киев",     "одинадцять",    "Structur 4",  "Address 4",  Y{"Inecca", "Deeva",   []string{"Photos", "Reading", "Other Hoobyt","Flyting", "Productive"}}},
            }
-	
 
-// Прокртука структуры	
+          fmt.Println("Other [1] : ",t[1].Other)
+
+// Обход структуры	
 for _, tt:=range(t){
      fmt.Println(tt.Structure, tt.Other, tt.Tg.Name, tt.Tg.Tags[0], len(tt.Tg.Tags))
          
          // Добавление тега в массив
-         tt.Tg.Tags=append(tt.Tg.Tags,"Добавленный тег")
-
+         tt.Tg.Tags = append(tt.Tg.Tags, "Добавленный тег")
+         tt.Tg.Tags = append(tt.Tg.Tags, "Составный тег ")
+         tt.Tg.Tags = append(tt.Tg.Tags, "Новый тег ")
+         tt.Tg.Tags = append(tt.Tg.Tags, "Интеграционный тег ")
+         tt.Tg.Tags = append(tt.Tg.Tags, "Компаудный тег ")
+         tt.Tg.Tags = append(tt.Tg.Tags, "Системный тег ")
+         
          // Прокурутка Tags
          for _, iop:=range(tt.Tg.Tags){
              fmt.Println("Tags :", iop)
@@ -42,3 +48,5 @@ for _, tt:=range(t){
        fmt.Println( "\n")
  }
 }
+
+// © 2022 GitHub, In
