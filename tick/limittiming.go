b3b1d9eb37327e95a5df96cfa22184c23d08a896
Arthur Savage <alerc@meta.ua>
2022-09-12 10:30:27 +0300

    Create limittiming.go

---
+++ tick/limittiming.go
@@ -0,0 +1,32 @@
+package main
+ 
+import (
+	"fmt"
+	"time"
+)
+ 
+func main() {
+ 
+	to := time.After(3 * time.Second)
+	list := make([]string, 0)
+	done := make(chan bool, 1)
+ 
+	fmt.Println("Начало вставки элементов")
+	go func() {
+		defer fmt.Println("Выход из горутины")
+		for {
+			select {
+			case <-to:
+				fmt.Println("Время истекло")
+				done <- true
+				return
+			default:
+				list = append(list, time.Now().String())
+			}
+		}
+	}()
+ 
+	<-done
+	fmt.Printf("Получилось вставить %d элементов\n", len(list))
+ 
+}
