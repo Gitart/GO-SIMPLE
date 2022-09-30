5b067d34ca6369758ac6ceafee4afac359142791
Arthur Savage <alerc@meta.ua>
2022-09-30 15:31:27 +0300

    Update interfaceparse.go

--- interface/interfaceparse.go
+++ interface/interfaceparse.go
@@ -2,28 +2,23 @@ package main
 
 import "fmt"
 
-
-type Ui struct{
+type Ui struct {
 	Name, Location string
-
 }
 
 type User struct {
 	Id             int
 	Name, Location string
-	ss []int
-	kk []Ui
+	ss             []int
+	kk             []Ui
 }
 
 type Player struct {
 	User
 	GameId int
-	fff [] string
-	ff []int
-	
+	fff    []string
+	ff     []int
 }
-
-
 
 func main() {
 	p := Player{}
@@ -31,13 +26,12 @@ func main() {
 	p.Name = "Matt"
 	p.Location = "LA"
 	p.GameId = 90404
-	p.fff=[]string{"fff","ggg"}
-	p.ff=[]int{12,444,555,3332}
-	p.User.ss=[]int{12,444,225,333211}
-//	p.User.kk[0]=Ui{"ddd","fff"}
-//	p.User.kk[1]=Ui{"ddd","fff"}
-	p.User.kk=[]Ui{{"ddd","fff"},{"ddd","fff"},{"ddd","fff"},{"ddd","fff"},{"ddd","fff"},{"ddd","fff"}}
+	p.fff = []string{"fff", "ggg"}
+	p.ff = []int{12, 444, 555, 3332}
+	p.User.ss = []int{12, 444, 225, 333211}
+	//	p.User.kk[0]=Ui{"ddd","fff"}
+	//	p.User.kk[1]=Ui{"ddd","fff"}
+	p.User.kk = []Ui{{"ddd", "fff"}, {"ddd", "fff"}, {"ddd", "fff"}, {"ddd", "fff"}, {"ddd", "fff"}, {"ddd", "fff"}}
 
-	
 	fmt.Printf("%+v", p)
 }
