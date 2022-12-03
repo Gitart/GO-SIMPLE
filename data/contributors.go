b51f8e7feacec9525b54f997b084770f881aa8e5
Arthur Savage <alerc@meta.ua>
2022-12-03 15:18:06 +0300

    Update contributors.go

--- data/contributors.go
+++ data/contributors.go
@@ -1,9 +1,9 @@
-"Carl Chatfield <carlchatfield@gmail.com>"
-"Carl Jackson <carl@stripe.com>"
-"Carl Johnson <me@carlmjohnson.net>"
-"Carl Mastrangelo <notcarl@google.com>"
-"Carl Shapiro <cshapiro@google.com> <cshapiro@golang.org>"
-"Carlos Castillo <cookieo9@gmail.com>"
-"Carlos Cirello <uldericofilho@gmail.com>"
-"Carlos Eduardo Seo <cseo@linux.vnet.ibm.com>"
-"Dustin Carlino <dcarlino@google.com>"
+Carl Chatfield <carlchatfield@gmail.com>
+Carl Jackson <carl@stripe.com>
+Carl Johnson <me@carlmjohnson.net>
+Carl Mastrangelo <notcarl@google.com>
+Carl Shapiro <cshapiro@google.com> <cshapiro@golang.org>
+Carlos Castillo <cookieo9@gmail.com>
+Carlos Cirello <uldericofilho@gmail.com>
+Carlos Eduardo Seo <cseo@linux.vnet.ibm.com>
+Dustin Carlino <dcarlino@google.com>
