85a37676d121c117c8af7e4eb750e1474a73bc4c
Arthur Savage <alerc@meta.ua>
2022-12-03 15:07:15 +0300

    Create contributors.go

---
+++ data/contributors.go
@@ -0,0 +1,13 @@
+package project
+
+var Contributors = []string{
+	"Carl Chatfield <carlchatfield@gmail.com>",
+	"Carl Jackson <carl@stripe.com>",
+	"Carl Johnson <me@carlmjohnson.net>",
+	"Carl Mastrangelo <notcarl@google.com>",
+	"Carl Shapiro <cshapiro@google.com> <cshapiro@golang.org>",
+	"Carlos Castillo <cookieo9@gmail.com>",
+	"Carlos Cirello <uldericofilho@gmail.com>",
+	"Carlos Eduardo Seo <cseo@linux.vnet.ibm.com>",
+	"Dustin Carlino <dcarlino@google.com>",
+}
