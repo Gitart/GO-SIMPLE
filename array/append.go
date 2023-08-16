4869d83e1f0c8063fbbaa848791350a3023f0ec6
Arthur Savage <alerc@meta.ua>
2023-08-16 15:42:45 +0300

    Update append.go

--- array/append.go
+++ array/append.go
@@ -1,30 +1,20 @@
 // a := []int{1, 2}
 // b := []int{11, 22}
 // a = append(a, b...) // a == [1 2 11 22]
-
-
 package main
-
 import (
 	"fmt"
-
-	
 )
 
 func main() {
-     
 SetG("fff","fff")
-
-
 }
 
 
 func SetG(keyval ...string) {
-         
 for _, s:=range keyval{
    fmt.Println(s)
 } 
-
 	kv := append([]string{"gl-go", "ddd"}, keyval...)
 	fmt.Println(kv)
 }
